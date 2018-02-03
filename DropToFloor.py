a795b4f33e668c472baa9fb91094c54ecd338bdb
IsseiMori <ganntyu1225issei@gmail.com>
2018-02-02 17:00:19 -0800

    outline finished

--- DropToFloor.py
+++ DropToFloor.py
@@ -2,7 +2,17 @@ from maya import cmds
 
 #drop the selected objects to the ground with offset
 def dropToFloor(nodes = None, offset = 0):
-    pass
+    #no object is selected, return error
+
+    #for loop for each object
+
+    #get the bouding box
+
+    #store minimum y value
+
+    #calculate the difference between the floor with offset
+
+    #move the object
 
 #window class to open window
 #call this class to run the script
@@ -18,5 +28,9 @@ class Window(object):
     
     #UI implementation
     def buildUI(self):
-        pass
+        #set up column
+
+        #offset value input box
+
+        #Run button
 
