2fff340341f3641de3f31bf015df14c7e7752e9e
IsseiMori <ganntyu1225issei@gmail.com>
2018-02-02 16:48:32 -0800

    added DropToFloor.py

---
+++ DropToFloor.py
@@ -0,0 +1,22 @@
+from maya import cmds
+
+#drop the selected objects to the ground with offset
+def dropToFloor(nodes = None, offset = 0):
+    pass
+
+#window class to open window
+#call this class to run the script
+class Window(object):
+    def __init__(self, name):
+        #close the current window if the same name window exists
+
+        #open window
+
+        #build UI
+
+        #show window
+    
+    #UI implementation
+    def buildUI(self):
+        pass
+
